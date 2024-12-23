b9d0f49aa752dd35ef65a8519a7f69a2cc38a504
Sebastian Vidal Aedo <sebastianvidalaedo@gmail.com>
2024-12-23 15:59:30 -0300

    update cards

--- src/components/Spread/Spread.js
+++ src/components/Spread/Spread.js
@@ -5,91 +5,114 @@ import TarotCard from '../TarotCard/TarotCard';
 const cards = [
   {
     name: 'The Fool',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Loco',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_fool.png',
   },
   {
     name: 'The Magician',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Mago',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_magician.png',
   },
   {
     name: 'The High Priestess',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Papisa',
+    image:
+      process.env.PUBLIC_URL + '/images/tarot-cards/the_high_priestess.png',
   },
   {
     name: 'The Empress',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Emperatriz',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_empress.png',
   },
   {
     name: 'The Emperor',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Emperador',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_emperor.png',
   },
   {
     name: 'The Hierophant',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Papa',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_hierophant.png',
   },
   {
     name: 'The Lovers',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'Los Enamorados',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_lovers.png',
   },
   {
     name: 'The Chariot',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Carro',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_chariot.png',
   },
   {
     name: 'Strength',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Fuerza',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/strength.png',
   },
   {
     name: 'The Hermit',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Ermitaño',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_hermit.png',
   },
   {
     name: 'Wheel of Fortune',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Rueda de la Fortuna',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/wheel_of_fortune.png',
   },
   {
     name: 'Justice',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Justicia',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/justice.png',
   },
   {
     name: 'The Hanged Man',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Colgado',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_hanged_man.png',
   },
   {
     name: 'Death',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Muerte',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/death.png',
   },
   {
     name: 'Temperance',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Templanza',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/temperance.png',
   },
   {
     name: 'The Devil',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Diablo',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_devil.png',
   },
   {
     name: 'The Tower',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Torre',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_tower.png',
   },
   {
     name: 'The Star',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Estrella',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_star.png',
   },
   {
     name: 'The Moon',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'La Luna',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_moon.png',
   },
   {
     name: 'The Sun',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Sol',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_sun.png',
   },
   {
     name: 'Judgement',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Juicio',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/judgment.png',
   },
   {
     name: 'The World',
-    image: process.env.PUBLIC_URL + '/images/tarot-cards/carta.png',
+    name_es: 'El Mundo',
+    image: process.env.PUBLIC_URL + '/images/tarot-cards/the_world.png',
   },
 ];
 
--- src/components/TarotCard/TarotCard.js
+++ src/components/TarotCard/TarotCard.js
@@ -4,7 +4,7 @@ import './TarotCard.css';
 const TarotCard = ({ card }) => (
   <div className="tarot-card">
     <img src={card.image} alt={card.name} />
-    <h3>{card.name}</h3>
+    <div>{card.name_es}</div>
   </div>
 );
 
